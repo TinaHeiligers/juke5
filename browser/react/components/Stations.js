8de794249f0beebd6d291acd09ca97dd11b4246c
TinaHeiligers <tina.heiligers@gmail.com>
2017-04-03 12:52:55 -0400

    wired up new stations component and container

---
+++ browser/react/components/Stations.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export default (props) => {
+	const stations = props.stations
+
+	return (
+		<div>
+			<h3>Stations</h3>
+				<div className="list-group">
+				{
+
+				Object.keys(stations).map(genre => {
+					return (
+						<div className="list-group-item" key={genre}>
+							<Link to={'fill/me/in/later'}>{genre}</Link>
+						</div>
+					);
+				})
+				}
+			</div>
+		</div>
+	);
+}
+
---
+++ browser/react/containers/StationsContainer.js
@@ -0,0 +1,52 @@
+import { connect } from 'react-redux';
+import Stations from '../components/Stations';
+
+const convertSongsToStations = function (songsArray) {
+
+	// console.log(songsArray);
+
+	const stations = {};
+	songsArray.forEach(song => {
+		const genre = song.genre;
+		stations[genre] = stations[genre] || [];
+		stations[genre].push(song);
+	});
+	return stations;
+};
+
+const mapStateToProps = function (state) {
+	return {
+		stations: convertSongsToStations(state.songs)
+	};
+};
+
+const mapDispatchToProps = function (dispatch) {
+	return {};
+};
+
+const StationsContainer = connect(
+      mapStateToProps,
+      mapDispatchToProps
+      )(Stations);
+
+export default StationsContainer;
+// const mapStateToProps = (state, ownProps) => {
+// 	return {
+// 		active: ownProps.filter === state.visibilityFilter
+// 	}
+// }
+
+// const mapDispatchToProps = (dispatch, ownProps) => {
+// 	return {
+// 		onClick: () => {
+// 			dispatch(setVisibilityFilter(ownProps.filter))
+// 		}
+// 	}
+// }
+
+// const FilterLink = connect(
+// 	mapStateToProps,
+// 	mapDispatchToProps
+// 	)(Link)
+
+// export default FilterLink;
